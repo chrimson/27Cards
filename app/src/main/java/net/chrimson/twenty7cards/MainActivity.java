0e7d454d87eea8a3ff8480fca093f6177adcd91c
Chris Limson <chrimson@gmail.com>
2024-09-23 15:58:48 -0400

    Layout

--- app/src/main/java/net/chrimson/twenty7cards/MainActivity.java
+++ app/src/main/java/net/chrimson/twenty7cards/MainActivity.java
@@ -3,12 +3,17 @@ package net.chrimson.twenty7cards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView c00 = findViewById(R.id.card00);
+//        c00.setText("foot");
+
         setContentView(R.layout.activity_main);
     }
 }
