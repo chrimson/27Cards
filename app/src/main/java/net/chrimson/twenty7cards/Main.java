a126270db50ccb4009f65f22d8c2e14ed398f065
Chris Limson <chrimson@gmail.com>
2024-09-23 21:37:46 -0400

    Loop

--- app/src/main/java/net/chrimson/twenty7cards/Main.java
+++ app/src/main/java/net/chrimson/twenty7cards/Main.java
@@ -27,50 +27,25 @@ public class Main {
       }
     }
 
-    // Round 1 of 3, place cards in respective columns
-    System.out.print("\n");
-    for (colPlan[0] = 0; colPlan[0] < 3; colPlan[0]++) {
-      System.out.printf("%2d: ", colPlan[0]);
-      for (colPlan[1] = 0; colPlan[1] < 3; colPlan[1]++) {
-        for (colPlan[2] = 0; colPlan[2] < 3; colPlan[2]++) {
-          Card listCard = cardCols[colPlan[0]][colPlan[1]][colPlan[2]];
-          System.out.printf("%2s %s  ", listCard.rank, listCard.suit);
+    // For 3 rounds, place cards in respective columns, each time selecting which columns
+    for (int round = 0; round < 3; round ++) {
+      System.out.print("\n");
+      int l = round % 3;
+      int m = (round + 1) % 3;
+      int n = (round + 2) % 3;
+      for (colPlan[l] = 0; colPlan[l] < 3; colPlan[l]++) {
+        System.out.printf("%2d: ", colPlan[l]);
+        for (colPlan[m] = 0; colPlan[m] < 3; colPlan[m]++) {
+          for (colPlan[n] = 0; colPlan[n] < 3; colPlan[n]++) {
+            Card listCard = cardCols[colPlan[0]][colPlan[1]][colPlan[2]];
+            System.out.printf("%2s %s  ", listCard.rank, listCard.suit);
+          }
         }
+        System.out.print("\n");
       }
       System.out.print("\n");
+      myCols[round] = input.nextInt();
     }
-    System.out.print("\n");
-    myCols[0] = input.nextInt();
-
-    // Round 2 of 3, place cards in respective columns
-    System.out.print("\n");
-    for (colPlan[1] = 0; colPlan[1] < 3; colPlan[1]++) {
-      System.out.printf("%2d: ", colPlan[1]);
-      for (colPlan[2] = 0; colPlan[2] < 3; colPlan[2]++) {
-        for (colPlan[0] = 0; colPlan[0] < 3; colPlan[0]++) {
-          Card listCard = cardCols[colPlan[0]][colPlan[1]][colPlan[2]];
-          System.out.printf("%2s %s  ", listCard.rank, listCard.suit);
-        }
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n");
-    myCols[1] = input.nextInt();
-
-    // Round 3 of 3, place cards in respective columns
-    System.out.print("\n");
-    for (colPlan[2] = 0; colPlan[2] < 3; colPlan[2]++) {
-      System.out.printf("%2d: ", colPlan[2]);
-      for (colPlan[0] = 0; colPlan[0] < 3; colPlan[0]++) {
-        for (colPlan[1] = 0; colPlan[1] < 3; colPlan[1]++) {
-          Card listCard = cardCols[colPlan[0]][colPlan[1]][colPlan[2]];
-          System.out.printf("%2s %s  ", listCard.rank, listCard.suit);
-        }
-      }
-      System.out.print("\n");
-    }
-    System.out.print("\n");
-    myCols[2] = input.nextInt();
 
     // Select user's card
     Card myCard = cardCols[myCols[0]][myCols[1]][myCols[2]];
