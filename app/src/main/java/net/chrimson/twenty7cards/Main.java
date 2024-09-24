087f0fef092dbe148fb3677fa1bc2bd717ecbda0
Chris Limson <chrimson@gmail.com>
2024-09-23 22:07:56 -0400

    Randomize

--- app/src/main/java/net/chrimson/twenty7cards/Main.java
+++ app/src/main/java/net/chrimson/twenty7cards/Main.java
@@ -8,8 +8,9 @@ public class Main {
     String[]        suits    = { "C", "D", "H", "S" };
     String[]        ranks    = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
     ArrayList<Card> deck     = new ArrayList<Card>();
-    Card[][][]      cardCols = new Card[3][3][3];
-    int[]           colPlan  = new int[3];
+    Card[][][]      cards = new Card[3][3][3];
+    int[]           plan  = new int[3];
+    ArrayList<Card> column = new ArrayList<Card>();
 
     // Construct a deck
     for (int suit = 0; suit < 4; suit++) {
@@ -19,36 +20,47 @@ public class Main {
     }
 
     // Choose 27 for the game, assigning columns for each round
-    for (colPlan[0] = 0; colPlan[0] < 3; colPlan[0]++) {
-      for (colPlan[1] = 0; colPlan[1] < 3; colPlan[1]++) {
-        for (colPlan[2] = 0; colPlan[2] < 3; colPlan[2]++) {
-          cardCols[colPlan[0]][colPlan[1]][colPlan[2]] = deck.remove((int)(Math.random() * deck.size()));
+    for (plan[0] = 0; plan[0] < 3; plan[0]++) {
+      for (plan[1] = 0; plan[1] < 3; plan[1]++) {
+        for (plan[2] = 0; plan[2] < 3; plan[2]++) {
+          cards[plan[0]][plan[1]][plan[2]] = deck.remove((int)(Math.random() * deck.size()));
         }
       }
     }
 
-    // For 3 rounds, place cards in respective columns, each time selecting which columns
+    // For 3 rounds, list cards in their columns, each time selecting which columns
     for (int round = 0; round < 3; round ++) {
       System.out.print("\n");
+
+      // Cycle order of rounds
       int l = round % 3;
       int m = (round + 1) % 3;
       int n = (round + 2) % 3;
-      for (colPlan[l] = 0; colPlan[l] < 3; colPlan[l]++) {
-        System.out.printf("%2d: ", colPlan[l]);
-        for (colPlan[m] = 0; colPlan[m] < 3; colPlan[m]++) {
-          for (colPlan[n] = 0; colPlan[n] < 3; colPlan[n]++) {
-            Card listCard = cardCols[colPlan[0]][colPlan[1]][colPlan[2]];
-            System.out.printf("%2s %s  ", listCard.rank, listCard.suit);
+
+      // Construct each column
+      for (plan[l] = 0; plan[l] < 3; plan[l]++) {
+        for (plan[m] = 0; plan[m] < 3; plan[m]++) {
+          for (plan[n] = 0; plan[n] < 3; plan[n]++) {
+            column.add(cards[plan[0]][plan[1]][plan[2]]);
           }
+        }
+
+        // Randomize column
+        System.out.printf("%2d:  ", plan[l]);
+        while (column.size() > 0) {
+          Card card = column.remove((int)(Math.random() * column.size()));
+          System.out.printf("%2s %s  ", card.rank, card.suit);
         }
         System.out.print("\n");
       }
       System.out.print("\n");
+
+      // User input column
       myCols[round] = input.nextInt();
     }
 
     // Select user's card
-    Card myCard = cardCols[myCols[0]][myCols[1]][myCols[2]];
+    Card myCard = cards[myCols[0]][myCols[1]][myCols[2]];
     System.out.printf("\n%2s %s !\n\n", myCard.rank, myCard.suit);
   }
 }
